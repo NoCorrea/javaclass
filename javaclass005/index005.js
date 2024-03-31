2366e51e16e75edf263d800873b2ab942d3225f9
Nonô Correa <correa.advg@gmail.com>
2024-03-31 01:30:02 -0300

    iniciando ex javaclass005

---
+++ javaclass005/index005.js
@@ -0,0 +1,12 @@
+/*window.document.write(window.document.URL)
+ mostra a url
+window.document.write(window.document.characterSet)
+mostra o charset usado
+window.document.write(window.navigator.appName)
+mostra o navegador*/
+var p1 = document.getElementsByTagName('p')[1]
+
+document.write(p1.innerHTML)
+var d = document.getElementById('msg')
+/* se usar o 'innertText' vai copiar o texto sem formatação*/
+
