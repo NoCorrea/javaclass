b5610a7e937093a778447e0bb9d0d7bd5c98031d
Nonô Correa <correa.advg@gmail.com>
2024-04-02 01:17:48 -0300

    inserindo exercidio javaclass006

---
+++ javaclass006/index.006.js
@@ -0,0 +1,16 @@
+var a = document.getElementById ('area')
+a.addEventListener('click',clicar)
+a.addEventListener('mouseenter',entrar)
+a.addEventListener('mouseout',sair)
+function clicar() {
+a.innerText = 'clicou'
+a.style.background ='red'
+}
+function entrar() {
+    a.innerText = 'entrou'  
+}
+function sair() {
+    a.innerText = 'saiu' 
+    a.style.background ='green' 
+}
+
