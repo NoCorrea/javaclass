f9e7fd4e6786711b2b9ebbc2c45bd1bf4fa16214
Nonô Correa <correa.advg@gmail.com>
2024-04-19 02:14:52 -0300

    finalizando aula 17 java

--- javaclass009/javaclass009.js
+++ javaclass009/javaclass009.js
@@ -1,3 +1,12 @@
-var nationality=getElemntById('nationality');
-var resultnationality=('result-nat'){
-    if addEventListener(input, )
+var nationality=document.getElementById('nationality');
+var resultnationality=document.getElementById('result-nat')
+
+
+nationality.addEventListener('input', function(){
+   if(nationality.value.toLowerCase() == 'brasil'){
+    resultnationality.textContent='Você é Brasileiro';}
+   else{
+    resultnationality.textContent= 'Você é estrangeiro';
+}
+});
+
